b0428956af0faa6eea862b69da1628b1619ba79b
omartins-zs <gabrielmatheus209@gmail.com>
2026-03-31 17:02:46 -0300

    :sparkles: feat: aplicar toggle flowbite no guest

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -22,11 +22,15 @@
     <div class="fixed top-4 right-4 z-20">
         <button
             type="button"
-            class="btn-secondary"
+            class="rounded-lg p-2.5 text-sm text-slate-500 transition-colors hover:bg-slate-100 focus:ring-4 focus:ring-slate-200 focus:outline-none dark:text-slate-400 dark:hover:bg-slate-700 dark:focus:ring-slate-700"
             id="theme-toggle"
             data-tippy-content="Alternar tema">
-            <i id="theme-toggle-dark-icon" class="fa-solid fa-moon hidden text-sm"></i>
-            <i id="theme-toggle-light-icon" class="fa-solid fa-sun hidden text-sm"></i>
+            <svg id="theme-toggle-dark-icon" class="hidden h-5 w-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
+            </svg>
+            <svg id="theme-toggle-light-icon" class="hidden h-5 w-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" clip-rule="evenodd" d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"></path>
+            </svg>
         </button>
     </div>
     <main class="flex min-h-screen items-center justify-center p-6">
